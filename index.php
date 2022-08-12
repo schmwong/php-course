fc46039d3e465ec531c91ed0b1d2fe37494092fe
schmwong <smwong87@gmail.com>
2022-08-12 12:36:33 +0800

    added `foreach` loop

--- index.php
+++ index.php
@@ -16,9 +16,24 @@
     $name = "PHP Store";
     $credit = 1000;
 
+    /* Arrays are groupings of information that can be 
+    tied to a variable. Arrays have keys and keys have values. */
+    $products['Computer']=750;
+    $products['Car']=15000;
+    $products['iPhone']=1000;
+    $products['Toaster']=75;
+
     // Using a period (.), we can concatenate text and variables.
     echo "<h1>Welcome to ".$name."!</h1>";
     echo "<h2>You have $".$credit." in your wallet.</h2>";
+
+    // You can access the value stored in an array like this:
+    // echo "<p>A car costs $".$products['Car']."</p>";
+
+    // Looping through the array
+    foreach($products as $key => $value){
+	    echo "<p>The ".$key." costs $".$value."</p>";
+    }
     ?>
   </body>
 </html>
